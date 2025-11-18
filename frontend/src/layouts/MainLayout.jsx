dc0174817d547726c6209cf8d0704e29476b8f67
WilliamKevim <kekegarangau@hotmail.com>
2025-11-18 18:34:19 -0300

    Ajuste do footer para ficar no final da página

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -8,7 +8,7 @@ function MainLayout() {
   const location = useLocation();
 
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
       <Header />
       <Breadcrumbs />
       <AnimatePresence mode="wait">
@@ -18,12 +18,13 @@ function MainLayout() {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -15 }}
           transition={{ duration: 0.3 }}
+          style={{ flex: 1 }}
         >
           <Outlet />
         </motion.main>
       </AnimatePresence>
       <Footer />
-    </>
+    </div>
   );
 }
 
